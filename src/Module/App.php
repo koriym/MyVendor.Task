8e1fe3e100f62f51f557e1f8358dd6eb187aa0dd
Akihito Koriyama <akihito.koriyama@gmail.com>
2016-02-25 11:58:17 +0900

    initial commit

---
+++ src/Module/App.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MyVendor\Task\Module;
+
+use BEAR\Sunday\Extension\Application\AbstractApp;
+
+class App extends AbstractApp
+{
+}
---
+++ src/Module/AppModule.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace MyVendor\Task\Module;
+
+use josegonzalez\Dotenv\Loader as Dotenv;
+use Koriym\DbAppPackage\DbAppPackage;
+use Ray\Di\AbstractModule;
+
+class AppModule extends AbstractModule
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        Dotenv::load([
+            'filepath' => dirname(dirname(__DIR__)) . '/.env',
+            'toEnv' => true
+        ]);
+        $this->install(new DbAppPackage($_ENV['DB_DSN'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_READ']));
+    }
+}
